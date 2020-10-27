5f582cadd09a6d01664802771a3b76522bb26f37
sophie-apples <sophieannemurray@gmail.com>
2020-10-27 16:36:20 +0000

    trying to fix error changed app.py file

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import os
 from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return "hello world"
+    return "Hello World ... again!"
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
