58e087739e1d92b0ccbbce3763cc34071188419f
sophie-apples <sophieannemurray@gmail.com>
2020-10-29 14:34:18 +0000

    stuck with edit tasks lesson

--- app.py
+++ app.py
@@ -13,16 +13,19 @@ app.config["MONGO_URI"] = 'mongodb+srv://Shashasha21:jk7nMqjBp6jvVDV@myfirstclus
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
 
+
 @app.route("/add_task")
 def add_task():
     return render_template("addtask.html",
-    categories=mongo.db.categories.find())
+        categories=mongo.db.categories.find())
+
 
 @app.route('/insert_task', methods=["POST"])
 def insert_task():
@@ -30,11 +33,27 @@ def insert_task():
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
+
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
-    the_task = mongo.db.tasks.find_one({"_id" : ObjectId(task_id)})
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     all_categories = mongo.db.categories.find()
-    return render_template('edittask.html', task=the_task, categories=all_categories)
+    category_list = [category for category in all_categories]
+    return render_template('edittask.html', task=the_task, categories = all_categories)
+
+
+@app.route('/update_task/<task_id>', methods=["POST"])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update({'_id': ObjectId(task_id)},
+        {
+            'task_name': request.form.get('task_name'),
+            'category_name': request.form.get('category_name'),
+            'task_description': request.form.get('task_description'),
+            'due_date': request.form.get('due_date'),
+        '   is_urgent': request.form.get('is_urgent')
+        })
+    return redirect(url_for('get_tasks'))
 
 
 if __name__ == "__main__":
